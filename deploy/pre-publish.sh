89ad06fea4301037ebea04448fa21f94c7dd87cf
Mr-jiangzhiguo <jiangzhiguo2010@qq.com>
2019-04-19 22:23:06 +0800

    fix(config): update pre-publish

--- deploy/pre-publish.sh
+++ deploy/pre-publish.sh
@@ -5,7 +5,7 @@ dockerImageName=${projectName}-${BUILD_TIMESTAMP}
 
 echo '*********************************** 准备打包从Github仓库拉取的代码...*********************************************'
 echo "**********${WORKSPACE}**********"
-tar --exclude .git --exclude *.tar.gz  -vzcf /tmp/${dockerImageName}.tar.gz -C  ${WORKSPACE} .  \
+tar --exclude=node_modules --exclude=.git --exclude='*.tar.gz'  -vzcf /tmp/${dockerImageName}.tar.gz -C  ${WORKSPACE} .  \
 && mv /tmp/${dockerImageName}.tar.gz  ${WORKSPACE} \
 && echo '*********************************** 打包成功 *********************************************' \
 && echo '************************************** 准备发送至服务器... **********************************************'
